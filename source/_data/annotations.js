43fa0f978bf41df511b19e8c33813b2d86762345
m-e-h <m-e-h@users.noreply.github.com>
2014-05-05 12:56:12 -0400

    Theme edits

--- source/_data/annotations.js
+++ source/_data/annotations.js
@@ -5,6 +5,11 @@ var comments = {
 		"title" : "Masthead",
 		"comment": "The main header of the site doesn't take up too much screen real estate in order to keep the focus on the core content. It's using a linear CSS gradient instead of a background image to give greater design flexibility and reduce HTTP requests."
 	},
+		{
+		"el": ".sg-colors",
+		"title" : "Colors",
+		"comment": "This initial color palette is based on the newly approve shield logo.</p>"
+	},
 	{
 		"el": ".logo",
 		"title" : "Logo",
